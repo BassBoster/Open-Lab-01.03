4211e1ba28a2c3ef2cd02acd4a7b503b7e176bae
Andrej Kováč <93122145+BassBoster@users.noreply.github.com>
2021-11-12 16:17:52 +0100

    vypracované

--- Open-Lab-01.03/Exercise.cs
+++ Open-Lab-01.03/Exercise.cs
@@ -7,6 +7,10 @@ namespace Open_Lab_01._03
         public int Addition(int number)
         {
             throw new NotImplementedException();
+            Console.WriteLine("zadaj číslo");
+            int vstup = Convert.ToInt32(Console.ReadLine());
+            int vystup = vstup + 1;
+            Console.WriteLine("výsledopk " + vystup);
         }
     }
 }
